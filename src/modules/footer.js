47da2da64cf9e7a7a1e7ee47c4ed1f1ec1ba01c0
JovaughnE <jovaughn.ewing@outlook.com>
2021-04-18 15:15:49 -0600

    Added a footer.js template.

---
+++ src/modules/footer.js
@@ -0,0 +1,9 @@
+class Footer { // create a class for the Footer
+    constructor(footerContent) {
+        this.footerContent = footerContent;
+
+    }
+}
+
+export const footer = new Footer ('Footer Content!'); //export the new class Footer as a variable
+
